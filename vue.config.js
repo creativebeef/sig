f00168ed1bbe7d6c647b660545221d76008c140b
Jamie <jamie@creativebeef.co>
2021-01-10 23:53:39 +0000

    one

--- vue.config.js
+++ vue.config.js
@@ -2,9 +2,32 @@ const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
   publicPath: isProd ? '/create/' : '/',
+
   chainWebpack: config => {
     const svgRule = config.module.rule('svg')
     svgRule.uses.clear()
     svgRule.use('vue-svg-loader').loader('vue-svg-loader')
+  },
+
+  pluginOptions: {
+    s3Deploy: {
+      registry: undefined,
+      awsProfile: 'default',
+      overrideEndpoint: false,
+      region: 'eu-west-2',
+      bucket: 'sig',
+      createBucket: true,
+      staticHosting: true,
+      staticIndexPage: 'index.html',
+      staticErrorPage: 'index.html',
+      assetPath: 'dist',
+      assetMatch: '**',
+      deployPath: '/',
+      acl: 'public-read',
+      pwa: false,
+      enableCloudfront: false,
+      pluginVersion: '4.0.0-rc3',
+      uploadConcurrency: 5
+    }
   }
 }
